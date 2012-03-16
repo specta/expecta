fcb91db238536949ceca5bcbe9c93b61b658240a
David Hart <david@hart-dev.com>
2012-03-16 12:18:11 +0100

    Fix ARC warning and avoid having to bridge id objects

--- src/matchers/EXPMatchers+beIdenticalTo.h
+++ src/matchers/EXPMatchers+beIdenticalTo.h
@@ -1,3 +1,9 @@
 #import "Expecta.h"
 
-EXPMatcherInterface(beIdenticalTo, (void *expected));
+EXPMatcherInterface(_beIdenticalTo, (void *expected));
+
+#if __has_feature(objc_arc)
+#define beIdenticalTo(expected) _beIdenticalTo((__bridge void*)expected)
+#else
+#define beIdenticalTo _beIdenticalTo
+#endif
