cd7f741ae2aefa4ccfe67e45c1c0d3981a051ffa
David Hart <david@hart-dev.com>
2012-05-21 18:40:03 +0200

    Fixed the beCloseTo macro and object to do a more-correct float comparison.
    Came from reading this really good article on float comparison: http://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/

--- src/matchers/EXPMatchers+beCloseTo.m
+++ src/matchers/EXPMatchers+beCloseTo.m
@@ -5,16 +5,25 @@ EXPMatcherImplementationBegin(_beCloseToWithin, (id expected, id within)) {
     prerequisite(^BOOL{
         return [actual isKindOfClass:[NSNumber class]] &&
 		[expected isKindOfClass:[NSNumber class]] &&
-		[within isKindOfClass:[NSNumber class]];
+		([within isKindOfClass:[NSNumber class]] || (within == nil));
     });
 
     match(^BOOL{
-        double actualValue = [actual doubleValue];
-        double expectedValue = [expected doubleValue];
-        double withinValue = [within doubleValue];
-        double lowerBound = expectedValue - withinValue;
-        double upperBound = expectedValue + withinValue;
-        return (actualValue > lowerBound) && (actualValue < upperBound);
+		double actualValue = [actual doubleValue];
+		double expectedValue = [expected doubleValue];
+		
+		if (within != nil) {
+			double withinValue = [within doubleValue];
+			double lowerBound = expectedValue - withinValue;
+			double upperBound = expectedValue + withinValue;
+			return (actualValue > lowerBound) && (actualValue < upperBound);
+		} else {
+			double diff = fabs(actualValue - expectedValue);
+			actualValue = fabs(actualValue);
+			expectedValue = fabs(expectedValue);
+			double largest = (expectedValue > actualValue) ? expectedValue : actualValue;
+			return (diff <= largest * FLT_EPSILON);
+		}
     });
 
     failureMessageForTo(^NSString *{
