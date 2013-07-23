4aedec7e48a9f4388df331e388cdd3785b107647
segiddins <segiddins@segiddins.me>
2013-07-23 17:34:37 -0400

    Add Generic count Compatibility to haveCountOf

--- src/matchers/EXPMatchers+haveCountOf.m
+++ src/matchers/EXPMatchers+haveCountOf.m
@@ -2,10 +2,7 @@
 
 EXPMatcherImplementationBegin(haveCountOf, (NSUInteger expected)) {
   BOOL actualIsCompatible = [actual isKindOfClass:[NSString class]] ||
-    [actual isKindOfClass:[NSArray class]] ||
-  [actual isKindOfClass:[NSSet class]] ||
-  [actual isKindOfClass:(NSClassFromString(NSOrderedSet) ?: [NSArray class])] ||
-  [actual isKindOfClass:[NSDictionary class]];
+    [actual respondsToSelector:@selector(count)];
 
   prerequisite(^BOOL{
     return actualIsCompatible;
