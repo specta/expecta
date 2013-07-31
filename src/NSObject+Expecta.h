3f93e8a984618b36c08deb2104bd027af544111b
Tim Brückmann <tim@tibr.me>
2013-07-31 11:17:58 +0200

    Add XCTest support
    XCTest uses recordFailureWithDescription:inFile:atLine:expected:
    instead of failWithException:
    
    (cherry picked from commit 0f2a9f5c165d5788d05256bc819905b785b6f797)

--- src/NSObject+Expecta.h
+++ src/NSObject+Expecta.h
@@ -2,4 +2,8 @@
 
 @interface NSObject (Expecta)
 - (void)failWithException:(NSException *)exception;
+- (void)recordFailureWithDescription:(NSString *)description
+                              inFile:(NSString *)filename
+                              atLine:(NSUInteger)lineNumber
+                            expected:(BOOL)expected;
 @end
