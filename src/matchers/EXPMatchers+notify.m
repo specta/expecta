7c2365105496d926efc7f90f02e249899bbd6937
Ron Lisle <ron@lisles.net>
2013-11-16 11:56:20 +0530

    Reindent to 2 spaces per contribution guidelines

--- src/matchers/EXPMatchers+notify.m
+++ src/matchers/EXPMatchers+notify.m
@@ -1,69 +1,69 @@
 #import "EXPMatchers+notify.h"
 
 EXPMatcherImplementationBegin(notify, (id expected)){
-    BOOL actualIsNil = (actual == nil);
-    BOOL expectedIsNil = (expected == nil);
-    BOOL isNotification = [expected isKindOfClass:[NSNotification class]];
-    BOOL isName = [expected isKindOfClass:[NSString class]];
+  BOOL actualIsNil = (actual == nil);
+  BOOL expectedIsNil = (expected == nil);
+  BOOL isNotification = [expected isKindOfClass:[NSNotification class]];
+  BOOL isName = [expected isKindOfClass:[NSString class]];
+  
+  __block NSString *expectedName;
+  __block NSString *gotName = @"none";
+  __block BOOL expectedNotificationOccurred = NO;
+  __block id observer;
+  
+  prerequisite(^BOOL{
+    expectedNotificationOccurred = NO;
+    if (actualIsNil || expectedIsNil) return NO;
+    if (isNotification) {
+      expectedName = [expected name];
+    }else if(isName) {
+      expectedName = expected;
+    }else{
+      return NO;
+    }
     
-    __block NSString *expectedName;
-    __block NSString *gotName = @"none";
-    __block BOOL expectedNotificationOccurred = NO;
-    __block id observer;
-
-    prerequisite(^BOOL{
-        expectedNotificationOccurred = NO;
-        if (actualIsNil || expectedIsNil) return NO;
-        if (isNotification) {
-            expectedName = [expected name];
-        }else if(isName) {
-            expectedName = expected;
-        }else{
-            return NO;
+    observer = [[NSNotificationCenter defaultCenter] addObserverForName:nil object:nil queue:nil usingBlock:^(NSNotification *note){
+      gotName = note.name;
+      if (isNotification) {
+        expectedNotificationOccurred = [expected isEqual:note];
+      }else{
+        if ([gotName isEqualToString:expectedName]) {
+          expectedNotificationOccurred = YES;
         }
-        
-        observer = [[NSNotificationCenter defaultCenter] addObserverForName:nil object:nil queue:nil usingBlock:^(NSNotification *note){
-                gotName = note.name;
-                if (isNotification) {
-                    expectedNotificationOccurred = [expected isEqual:note];
-                }else{
-                    if ([gotName isEqualToString:expectedName]) {
-                        expectedNotificationOccurred = YES;
-                    }
-                }
-                // Do we want to fail if other occurs?
-        }];
-        ((EXPBasicBlock)actual)();
-        return YES;
-    });
-    
-    match(^BOOL{
-        if (!(isNotification || isName)) return NO;
-        if(expectedNotificationOccurred) {
-            [[NSNotificationCenter defaultCenter] removeObserver:observer];
-        }
-        return expectedNotificationOccurred;
-    });
-    
-    failureMessageForTo(^NSString *{
-        if (observer) {
-            [[NSNotificationCenter defaultCenter] removeObserver:observer];
-        }
-        if(actualIsNil) return @"the actual value is nil/null";
-        if(expectedIsNil) return @"the expected value is nil/null";
-        if(!(isNotification || isName)) return @"the actual value is not a notification or string";
-        return [NSString stringWithFormat:@"expected: %@, got: %@",expectedName, gotName];
-    });
-    
-    failureMessageForNotTo(^NSString *{
-        if (observer) {
-            [[NSNotificationCenter defaultCenter] removeObserver:observer];
-        }
-        if(actualIsNil) return @"the actual value is nil/null";
-        if(expectedIsNil) return @"the expected value is nil/null";
-        if(!(isNotification || isName)) return @"the actual value is not a notification or string";
-        return [NSString stringWithFormat:@"expected: none, got: %@", gotName];
-    });
+      }
+      // Do we want to fail if other occurs?
+    }];
+    ((EXPBasicBlock)actual)();
+    return YES;
+  });
+  
+  match(^BOOL{
+    if (!(isNotification || isName)) return NO;
+    if(expectedNotificationOccurred) {
+      [[NSNotificationCenter defaultCenter] removeObserver:observer];
+    }
+    return expectedNotificationOccurred;
+  });
+  
+  failureMessageForTo(^NSString *{
+    if (observer) {
+      [[NSNotificationCenter defaultCenter] removeObserver:observer];
+    }
+    if(actualIsNil) return @"the actual value is nil/null";
+    if(expectedIsNil) return @"the expected value is nil/null";
+    if(!(isNotification || isName)) return @"the actual value is not a notification or string";
+    return [NSString stringWithFormat:@"expected: %@, got: %@",expectedName, gotName];
+  });
+  
+  failureMessageForNotTo(^NSString *{
+    if (observer) {
+      [[NSNotificationCenter defaultCenter] removeObserver:observer];
+    }
+    if(actualIsNil) return @"the actual value is nil/null";
+    if(expectedIsNil) return @"the expected value is nil/null";
+    if(!(isNotification || isName)) return @"the actual value is not a notification or string";
+    return [NSString stringWithFormat:@"expected: none, got: %@", gotName];
+  });
 }
 
 EXPMatcherImplementationEnd
--- test/matchers/EXPMatchers+notifyTest.m
+++ test/matchers/EXPMatchers+notifyTest.m
@@ -7,87 +7,87 @@
 
 - (void)test_notify {
   assertPass(test_expect(^{
-      [[NSNotificationCenter defaultCenter] postNotificationName:@"testNotification1" object:nil];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"testNotification1" object:nil];
   }).to.notify(@"testNotification1"));
-
+  
   NSNotification *n = [[NSNotification alloc] initWithName:@"testNotification2" object:self userInfo:nil];
-
+  
   assertPass(test_expect(^{
-      [[NSNotificationCenter defaultCenter] postNotification:n];
+    [[NSNotificationCenter defaultCenter] postNotification:n];
   }).to.notify(n));
-
+  
   assertPass(test_expect(^{
     NSNotification *notification = [NSNotification
-        notificationWithName:@"NotificationName" object:nil];
-      
-    [[NSNotificationQueue defaultQueue]
-         enqueueNotification:notification
-         postingStyle:NSPostWhenIdle
-         coalesceMask:NSNotificationCoalescingOnName
-         forModes:nil];
-      
-  }).will.notify(@"NotificationName"));
+              notificationWithName:@"NotificationName" object:nil];
     
+    [[NSNotificationQueue defaultQueue]
+     enqueueNotification:notification
+     postingStyle:NSPostWhenIdle
+     coalesceMask:NSNotificationCoalescingOnName
+     forModes:nil];
+    
+  }).will.notify(@"NotificationName"));
+  
   assertFail(test_expect(^{
     // no notification
   }).to.notify(@"testNotification2"),
              @"expected: testNotification2, got: none");
-
+  
   assertFail(test_expect(nil).to.notify(@"testNotification"),
-               @"the actual value is nil/null");
-    
+             @"the actual value is nil/null");
+  
   assertFail(test_expect(^{
-      // no notification
+    // no notification
   }).to.notify(nil),
-               @"the expected value is nil/null");
-
+             @"the expected value is nil/null");
+  
   assertFail(test_expect(^{
-      [[NSNotificationCenter defaultCenter] postNotificationName:@"testNotification1" object:nil];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"testNotification1" object:nil];
   }).to.notify(@"testNotification2"),
              @"expected: testNotification2, got: testNotification1");
-    
-    assertFail(test_expect(^{
-        // not doing anything
-    }).to.notify([[NSObject alloc] init]), @"the actual value is not a notification or string");
+  
+  assertFail(test_expect(^{
+    // not doing anything
+  }).to.notify([[NSObject alloc] init]), @"the actual value is not a notification or string");
 }
 
 - (void)test_toNot_notify {
   assertPass(test_expect(^{
-      // no notification
+    // no notification
   }).notTo.notify(@"testExpectaNotification1"));
-
+  
   assertFail(test_expect(^{
-      [[NSNotificationCenter defaultCenter] postNotificationName:@"testNotification1" object:nil];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"testNotification1" object:nil];
   }).toNot.notify(@"testNotification1"),
              @"expected: none, got: testNotification1");
-
-    assertPass(test_expect(^{
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"testNotification1" object:nil];
-    }).notTo.notify(@"testNotification2"));
-    
-    NSNotification *n1 = [[NSNotification alloc] initWithName:@"testNotification4" object:self userInfo:nil];
-    NSNotification *n2 = [[NSNotification alloc] initWithName:@"testNotification4" object:nil userInfo:nil];
-    assertPass(test_expect(^{
-        [[NSNotificationCenter defaultCenter] postNotification:n1];
-    }).toNot.notify(n2));
-
-    assertPass(test_expect(^{
-        // no notification
-    }).willNot.notify(@"NotificationName"));
-    
-    
-    assertFail(test_expect(nil).toNot.notify(@"testNotification"),
-               @"the actual value is nil/null");
-    
-    assertFail(test_expect(^{
-        // no notification
-    }).toNot.notify(nil),
-               @"the expected value is nil/null");
-
-    assertFail(test_expect(^{
-        // no notification
-    }).toNot.notify([[NSObject alloc] init]), @"the actual value is not a notification or string");
-    
+  
+  assertPass(test_expect(^{
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"testNotification1" object:nil];
+  }).notTo.notify(@"testNotification2"));
+  
+  NSNotification *n1 = [[NSNotification alloc] initWithName:@"testNotification4" object:self userInfo:nil];
+  NSNotification *n2 = [[NSNotification alloc] initWithName:@"testNotification4" object:nil userInfo:nil];
+  assertPass(test_expect(^{
+    [[NSNotificationCenter defaultCenter] postNotification:n1];
+  }).toNot.notify(n2));
+  
+  assertPass(test_expect(^{
+    // no notification
+  }).willNot.notify(@"NotificationName"));
+  
+  
+  assertFail(test_expect(nil).toNot.notify(@"testNotification"),
+             @"the actual value is nil/null");
+  
+  assertFail(test_expect(^{
+    // no notification
+  }).toNot.notify(nil),
+             @"the expected value is nil/null");
+  
+  assertFail(test_expect(^{
+    // no notification
+  }).toNot.notify([[NSObject alloc] init]), @"the actual value is not a notification or string");
+  
 }
 
 @end
