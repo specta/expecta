677131be47bd20d0dbd4b2f38a097bb99bba0ae8
André Schneider <hello@andreschneider.me>
2014-03-05 18:37:05 +0100

    Added a Xcode template that contains the base Specta/Expecta spec structure with all required imports.

---
+++ Xcode Templates/Expecta.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Specta/Specta.h>
+#define EXP_SHORTHAND
+#import <Expecta/Expecta.h>
+#import "___VARIABLE_classToTest:identifier___.h"
+
+SpecBegin(___VARIABLE_classToTest:identifier___)
+
+SpecEnd
