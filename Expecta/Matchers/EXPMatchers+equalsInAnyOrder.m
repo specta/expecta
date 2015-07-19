2a129bc3ca8a934603914081eee44f0a7dbaf592
Joe Burgess <joe@flatironschool.com>
2015-07-18 21:12:59 -0400

    Fixing actual/expected mixup

--- Expecta/Matchers/EXPMatchers+equalsInAnyOrder.m
+++ Expecta/Matchers/EXPMatchers+equalsInAnyOrder.m
@@ -2,19 +2,18 @@
 
 EXPMatcherImplementationBegin(equalInAnyOrder, (id expected)) {
   BOOL actualIsCompatible = [actual isKindOfClass:[NSArray class]];
-  BOOL expectedIsCompatible = [expected isKindOfClass:[NSArray class]];
   BOOL expectedIsNil = (expected == nil);
 
+  BOOL sameClass = [expected isKindOfClass:[actual class]];
   prerequisite(^BOOL{
-    return actualIsCompatible && !expectedIsNil && expectedIsCompatible;
+    return actualIsCompatible && !expectedIsNil;
   });
 
   match(^BOOL{
     if(!actualIsCompatible) return NO;
-    if(!expectedIsCompatible) return NO;
 
     NSSet *actualSet = [NSSet setWithArray:actual];
-    NSSet *expectedSet = [NSSet setWithArray:actual];
+    NSSet *expectedSet = [NSSet setWithArray:expected];
 
     if (![actualSet isEqualToSet:expectedSet]) return NO;
 
@@ -23,22 +22,20 @@ EXPMatcherImplementationBegin(equalInAnyOrder, (id expected)) {
 
   failureMessageForTo(^NSString *{
     if(!actualIsCompatible) return [NSString stringWithFormat:@"%@ is not an instance of NSArray", EXPDescribeObject(actual)];
-    if(!expectedIsCompatible) return [NSString stringWithFormat:@"%@ is not an instance of NSArray", EXPDescribeObject(expected)];
 
     if(expectedIsNil) return @"the expected value is nil/null";
 
 
-    return [NSString stringWithFormat:@"expected %@ to be a equal any order of %@", EXPDescribeObject(actual), EXPDescribeObject(expected)];
+    return [NSString stringWithFormat:@"expected %@ to be equal in any order to %@", EXPDescribeObject(actual), EXPDescribeObject(expected)];
   });
 
   failureMessageForNotTo(^NSString *{
     if(!actualIsCompatible) return [NSString stringWithFormat:@"%@ is not an instance of NSArray", EXPDescribeObject(actual)];
-    if(!expectedIsCompatible) return [NSString stringWithFormat:@"%@ is not an instance of NSArray", EXPDescribeObject(expected)];
 
     if(expectedIsNil) return @"the expected value is nil/null";
 
 
-    return [NSString stringWithFormat:@"expected %@ to not be equal in any order of %@", EXPDescribeObject(actual), EXPDescribeObject(expected)];
+    return [NSString stringWithFormat:@"expected %@ to not be equal in any order to %@", EXPDescribeObject(actual), EXPDescribeObject(expected)];
   });
 }
 EXPMatcherImplementationEnd
